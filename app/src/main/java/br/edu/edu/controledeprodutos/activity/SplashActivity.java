e568dcfd76e06eb2c5f84f44812386dcd4d207bf
Estevamvga <estevam.pereira@outlook.com>
2022-06-17 00:14:01 -0300

    Commit autenticacao

---
+++ app/src/main/java/br/edu/edu/controledeprodutos/activity/SplashActivity.java
@@ -0,0 +1,14 @@
+package br.edu.edu.controledeprodutos.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
